80daeabfe9a90342c82461aefecbb9aa5280bf4f
DenisaCazacu <denisa.cazacu01@gmail.com>
2025-03-06 14:58:09 +0200

    first commit

--- Lab1/src/Lab1/Main.java
+++ Lab1/src/Lab1/Main.java
@@ -1,131 +1,124 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Lab1;
-
-/**
- *
- * @author Ecaterina, Denisa
- */
 
 import java.util.ArrayList;
 
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public void main(String[] args) {
+    public static void main(String[] args) {
         electric electricCar1 = new electric();
         electric electricCar2 = new electric();
         passenger passenger1 = new passenger();
-        passenger.setName("Denisa");
-        passenger.setAge(20);
+        passenger1.setName("Denisa");
+        passenger1.setAge(20);
         electricCar1.addPassenger(passenger1);
         electricCar2.addPassenger("Cati", 20);
         electricCar1.detail_car();
     }
-
 }
 
-abstract class car
-{
-
+abstract class car {
     Driver driver;
-    ArrayList<passenger> passengers;
+    ArrayList<passenger> passengers = new ArrayList<>();
     abstract void detail_car();
 }
-class electric extends car
-{
-    void detail_car()
-    {
-        System.out.print( "This is an electric car.");
-        System.out.println();
+
+class electric extends car {
+    void detail_car() {
+        System.out.println("This is an electric car.");
+        if (driver != null) {
+            driver.displayDriver();
+        }
+        System.out.println("Passengers: ");
+        for (passenger p : passengers) {
+            p.displayPassenger();
+        }
     }
-    void addPassenger (passenger p){
+
+    void addPassenger(passenger p) {
         passengers.add(p);
     }
-    void addPassenger (String name, int age){
+
+    void addPassenger(String name, int age) {
         passenger p = new passenger();
         p.setName(name);
         p.setAge(age);
         passengers.add(p);
     }
-    void addDriver (String name, int years){
+
+    void addDriver(String name, int years) {
         driver = new Driver();
         driver.setName(name);
         driver.setYearsExp(years);
     }
 }
-class internalC extends car
-{
-    void detail_car()
-    {
-        System.out.print( "This is an internal combustion car.");
-        this.driver.displayDriver();
+
+class internalC extends car {
+    void detail_car() {
+        System.out.println("This is an internal combustion car.");
+        if (driver != null) {
+            driver.displayDriver();
+        } else {
+            System.out.println("No driver assigned.");
+        }
         System.out.println("Passengers: ");
-        for(passenger p : passengers){
+        for (passenger p : passengers) {
             p.displayPassenger();
         }
     }
 }
-class person
-{
+
+class person {
     private String name;
-    public void setName(String name)
-    {
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getName()
-    {
+
+    public String getName() {
         return this.name;
     }
-    public void SayHi()
-    {
-        System.out.print( "Hi, I am a person!");
-    }
 
+    public void SayHi() {
+        System.out.println("Hi, I am a person!");
+    }
 }
 
-class Driver extends person
-{
-    private  int yearsExp;
-    public void setYearsExp(int yearExp)
-    {
-        this.yearsExp=yearsExp;
+class Driver extends person {
+    private int yearsExp;
+
+    public void setYearsExp(int yearExp) {
+        this.yearsExp = yearExp;
     }
-    public int getYearsExp()
-    {
+
+    public int getYearsExp() {
         return this.yearsExp;
     }
 
-    public void SayHi()
-    {
-        System.out.print( "Hi, I am a driver!");
+    public void SayHi() {
+        System.out.println("Hi, I am a driver!");
     }
-    void displayDriver(){
+
+    void displayDriver() {
         System.out.println("Driver Name: " + this.getName());
-        System.out.println("Driver's Experience:" + yearsExp);
+        System.out.println("Driver's Experience: " + yearsExp);
     }
 }
 
-class passenger extends person
-{
+class passenger extends person {
     private int age;
-    public void setAge(int age)
-    {
-        this.age=age;
+
+    public void setAge(int age) {
+        this.age = age;
     }
-    public int getAge()
-    {
+
+    public int getAge() {
         return this.age;
     }
-    public void SayHi()
-    {
-        System.out.print( "Hi i am a passenger! ");
+
+    public void SayHi() {
+        System.out.println("Hi, I am a passenger!");
     }
-    public void displayPassenger(){
+
+    public void displayPassenger() {
         System.out.println("Passenger Name: " + this.getName());
         System.out.println("Passenger Age: " + age);
     }
---
+++ Lab2/README.md.txt
@@ -0,0 +1,5 @@
+Team composition:
+Cazacu Denisa
+Turcu Ecaterina
+Craciun Daniel
+Shahin Wissam
